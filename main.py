579fec09f53ee863b1e886a8bcebe2e7cbbe9a96
Vincent Larkin <vinlark@outlook.com>
2018-10-11 15:44:47 -0500

    Adding CSV Creating....kinda

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import subprocess, sys, os, shutil, csv
 def ammoneo():
 	print("R - Read Reminder List")
+	print("N - New Reminder List")
+	print("X - Exit")
 	while True:
 		main = input("> ")
 
@@ -9,6 +11,29 @@ def ammoneo():
 			f = open(read)
 			reader = csv.reader(f)
 			for row in reader:
-				print(row)
+				print (','.join(row))
+
+		if main == "N":
+			new = input("Name of New List: ")
+			firstremind = input("First Reminder: ")
+			with open(new, 'wb') as csvfile:
+				filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+				filewriter.writerow([firstremind])
+			#write = input("Name of New List: ")
+			#ifile  = open(write, "rb")
+			#reader = csv.reader(write)
+			#ofile  = open(write, "wb")
+			#writer = csv.writer(ofile, quotechar='"', quoting=csv.QUOTE_ALL)
+			#for row in reader:
+			#	writer.writerow(row)
+			#	ifile.close()
+			#	ofile.close()
+
+
+		if main == "X":
+			exit()
+
+		else:
+			ammoneo()
 
 ammoneo()
